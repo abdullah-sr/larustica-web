00661064b0c48ed2e87ba7ecc5d471c78f326a9d
Abdullah <hrfssa@gmail.com>
2018-02-10 13:01:36 -0800

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const BUILD_DIR = path.resolve(__dirname, './dist');
+const APP_DIR = path.resolve(__dirname, './src');
+
+const config = {
+    entry: `${APP_DIR}/main.js`,
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, include: APP_DIR, use: 'babel-loader' },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `${APP_DIR}/index.html`,
+            filename: 'index.html',
+            hash: true,
+        }),
+        new HtmlWebpackPlugin({
+            template: `${APP_DIR}/gallery.html`,
+            filename: 'gallery.html',
+            hash: true,
+        }),
+        new UglifyJsPlugin(),
+    ],
+};
+
+module.exports = config;
