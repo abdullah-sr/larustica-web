b8848f7ef026e3e77a5f22aa7e55dabeba6d5259
Abdullah <hrfssa@gmail.com>
2018-02-18 22:35:19 -0800

    added menu

--- src/common.js
+++ src/common.js
@@ -1,8 +1,15 @@
-export default function() {
+export default function () {
+
+    if(window.innerWidth <= 905) {
+        document.querySelector(".nav-container").classList.add('shrink');
+        document.querySelector(".nav-ul").classList.add('collapse');
+    }
+
     let last_known_scroll_position = 0;
     let ticking = false;
 
     function shrinkHeader(scrollPos) {
+        if(window.innerWidth <= 905) return;
         if (scrollPos > 50) {
             document.querySelector(".nav-container").classList.add('shrink');
         } else {
@@ -19,5 +26,69 @@ export default function() {
             });
             ticking = true;
         }
+    });
+
+    const optimizedResize = (function () {
+
+        let callbacks = [],
+            running = false;
+
+        // fired on resize event
+        function resize() {
+
+            if (!running) {
+                running = true;
+
+                if (window.requestAnimationFrame) {
+                    window.requestAnimationFrame(runCallbacks);
+                } else {
+                    setTimeout(runCallbacks, 66);
+                }
+            }
+
+        }
+
+        // run the actual callbacks
+        function runCallbacks() {
+
+            callbacks.forEach(function (callback) {
+                callback();
+            });
+
+            running = false;
+        }
+
+        // adds callback to loop
+        function addCallback(callback) {
+
+            if (callback) {
+                callbacks.push(callback);
+            }
+
+        }
+
+        return {
+            // public method to add additional callback
+            add: function (callback) {
+                if (!callbacks.length) {
+                    window.addEventListener('resize', resize);
+                }
+                addCallback(callback);
+            }
+        }
+    }());
+
+
+    optimizedResize.add(function () {
+        if(window.innerWidth <= 905) {
+            document.querySelector(".nav-container").classList.add('shrink');
+            document.querySelector(".nav-ul").classList.add('collapse');
+        } else {
+            document.querySelector(".nav-ul").classList.remove('collapse');
+        }
+    });
+
+    document.getElementById('toggle-nav').addEventListener('click', function(){
+        document.querySelector('.nav-ul').classList.toggle('expand')
     });
 }
